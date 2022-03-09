0e4ad171aa0dabd386a2387f1bbedc3338ffa471
John Padilla <johnpadi8790@gmail.com>
2022-03-08 22:45:36 -0700

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by John Padilla on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
