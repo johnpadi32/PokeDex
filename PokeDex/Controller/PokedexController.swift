8b3eb06fcd00c9e0f79a2293b4186bbbed74c53d
John Padilla <johnpadi8790@gmail.com>
2022-03-09 01:49:01 -0700

    PokedexController UI
    Building the UI for the mainScreen

---
+++ PokeDex/Controller/PokedexController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by John Padilla on 3/8/22.
+//
+
+import UIKit
+
+private let identifier = "PokedexCell"
+
+class PokedexController: UICollectionViewController {
+    
+    //MARK: - Properties
+    
+    let imageView: UIImageView = {
+       let iv = UIImageView()
+        iv.image = UIImage(named: "backTest")
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    
+    //MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureNavBar()
+        configureCollectionView()
+    }
+
+    //MARK: - Helpers
+    
+    func configureNavBar() {
+        
+        collectionView.backgroundView = imageView
+        title = "Pokedex"
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .mainRed()
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .done, target: self, action: #selector(handleSearch)),
+            UIBarButtonItem(image: UIImage(systemName: "star"), style: .done, target: self, action: nil)
+        ]
+    }
+    
+    func configureCollectionView() {
+        collectionView.register(PokedexCell.self, forCellWithReuseIdentifier: identifier)
+
+    }
+    
+    //MARK: - Actions
+    
+    @objc func handleSearch() {
+        print("Search Prokemon")
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension PokedexController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! PokedexCell
+        
+        return cell
+    }
+}
+
+
+//MARK: - UICollectionViewDelegateFlowLayout
+
+extension PokedexController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width / 2) - 20, height: 110)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 15, left: 10, bottom: 10, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+}
---
+++ PokeDex/View/PokedexCell.swift
@@ -0,0 +1,80 @@
+//
+//  PokedexCell.swift
+//  PokeDex
+//
+//  Created by John Padilla on 3/8/22.
+//
+
+import UIKit
+
+class PokedexCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    
+    let imageview: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(named: "pikachu")
+        return iv
+    }()
+    
+    let nameLabel: UILabel = {
+       let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 19, weight: .bold)
+        label.textAlignment = .left
+        label.text = "Picachu"
+        return label
+    }()
+    
+    let typeLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Electric"
+        label.textColor = .lightGray
+        label.backgroundColor = .systemGray4
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let backView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .systemGray6
+        return view
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    //MARK: - Helpers
+    
+    func configureUI() {
+        
+        self.layer.cornerRadius = 15
+        self.clipsToBounds = true
+        
+        addSubview(backView)
+        backView.fillSuperview()
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 2, paddingLeft: 5, paddingRight: 5, height: 40)
+        
+        addSubview(imageview)
+        imageview.anchor(top: topAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 10, paddingBottom: 0, paddingRight: 0, width: 70, height: 70)
+        
+        addSubview(typeLabel)
+        typeLabel.anchor(left: leftAnchor, bottom: bottomAnchor, paddingLeft: 5, paddingBottom: 13, width: 75, height: 25)
+        
+    }
+}
